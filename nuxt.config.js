12d6295863b27f8c9c9bad14068ccb198c944d97
Roman Orlov <orlovrs@live.com>
2021-06-20 15:38:44 +0300

    add actions

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,8 @@
 export default {
+  target: 'static',
+  router: {
+    base: '/api-testing/',
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'api-testing',
